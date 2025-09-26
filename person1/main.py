25a5fdc9ee79535b9caa42229ea767652024bbfa
Дима Глущенко <dimaglushchenko@MacBook-Air-Dima.local>
2025-09-26 10:43:11 +0300

    Удаляю папку person1 для чистого старта

--- person1/main.py
+++
@@ -1,5 +0,0 @@
-from utils import read_data
-
-if __name__ == "__main__":
-    data = read_data()
-    print("Данные из файла:", data)
--- person1/utils.py
+++
@@ -1,6 +0,0 @@
-def read_data():
-    try:
-        with open('person1/data.txt', 'r') as file:
-            return file.read()
-    except FileNotFoundError:
-        return "Файл не найден"
